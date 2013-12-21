41047e4104fc0943cf2e27f250e896102f51d969
Nopphadon Wangmethakul <w_lufe71@hotmail.com>
2013-12-21 16:37:30 +0700

    Add UI

--- src/com/example/pointofsale/AddActivity.java
+++ src/com/example/pointofsale/AddActivity.java
@@ -1,13 +1,171 @@
 package com.example.pointofsale;
 
-public class AddActivity {
+import dao.InventoryDB;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+public class AddActivity extends Activity {
+
+	private Button btnScan;
+	//DBCommandConfirm confirm = new DBCommandConfirm();
+	private TextView txtResult;
+	private EditText tItemID;
+	private EditText tName;
+	private EditText tQuantity;
+	private EditText tPrice;
+	private EditText tDescription;
+	
+	//Context context, String name, CursorFactory factory, int version
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add);
+		tItemID = (EditText) findViewById(R.id.txtItemID);
+		tName = (EditText) findViewById(R.id.txtName);
+		tQuantity = (EditText) findViewById(R.id.txtQuantity);
+		tPrice = (EditText) findViewById(R.id.txtPrice);
+		tDescription = (EditText) findViewById(R.id.txtDescription);
+		
+		// connect btnScan and txtResult to View
+		btnScan = (Button) findViewById(R.id.buttonScan);
+		txtResult = (EditText) findViewById(R.id.txtItemID);
+		
+		// Test
+//		final InventoryDB myDb = new InventoryDB( context,name, factory, version);
+	
+		final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+		final AlertDialog ad = adb.create();
+		
+		// btnSave (Save)
+
+		final Button save = (Button) findViewById(R.id.btnSave);
+		save.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				String productID = tItemID.getText().toString();
+				String productName = tName.getText().toString();
+				String productQuan = tQuantity.getText().toString();
+				String productPrice = tPrice.getText().toString();
+				String productDescription = tDescription.getText().toString();
+				if (productDescription == null) productDescription = "";
+				
+//			if (confirm.checkSaveData(productID,productName,productQuan,productPrice)) 
+//			{
+//				myDb.InsertData(productID, productName, productQuan, productPrice, productDescription);
+//				
+//				Toast.makeText(AddActivity.this, "Add Data Successfully. ",
+//						Toast.LENGTH_SHORT).show();
+//				Intent newActivity = new Intent(AddActivity.this,InventoryActivity.class);
+//				startActivity(newActivity);
+//			}
+			//	else {ad.setMessage("Invalid. A field is missing or the Product ID is already in used."); ad.show();}
+
+			}
+		}
+	);
+
+		// btnCancel (Cancel)
+		final Button cancel = (Button) findViewById(R.id.btnCancel);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Open Form Main
+				Intent newActivity = new Intent(AddActivity.this,
+						InventoryActivity.class);
+				startActivity(newActivity);
+				finish();
+			}
+		});
+
+		// define Event when press btnScan
+		btnScan.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				try {
+					// define intent to call Barcode Scanner
+					Intent intent = new Intent(
+							"com.google.zxing.client.android.SCAN");
+					// send Mode to Scan with Barcode Scanner
+					intent.putExtra("SCAN_MODE", "ONE_D_MODE");
+
+					// start Activity from intent which define by requestCode =
+					// 0
+					startActivityForResult(intent, 0);
+
+				} catch (Exception e) {
+					// TODO: handle exception
+					// if don't install Barcode Scanner there will push a
+					// message Please Install Barcode Scanner
+					Toast.makeText(getBaseContext(),
+							"Please Install Barcode Scanner",
+							Toast.LENGTH_SHORT).show();
+				}
+
+			}
+		});
+	}
+
+	// when complete the scan it will call function onActivityResult
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			Intent intent) {
 		// TODO Auto-generated method stub
+		if (requestCode == 0) // test requestCode and Barcode Scanner are same
+								// with value that we got
+		{
+			if (resultCode == RESULT_OK) // if Barcode Scanner complete
+			{
+				// Receive Barcode Scanner that got by scan
+				String contents = intent.getStringExtra("SCAN_RESULT");
+				// Receieve Barcode Scanner that got each type
+				String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+				// show in txtResult
+				txtResult.setText("" + contents);
+			}
+		}
 
+		// btnSave (Save)
+		/*
+		 * final Button save = (Button) findViewById(R.id.btnSave);
+		 * save.setOnClickListener(new View.OnClickListener() { public void
+		 * onClick(View v) {
+		 * 
+		 * // If Save Complete if (SaveData()) { // Open Form Main Intent
+		 * newActivity = new Intent(AddScreen.this, InventoryScreen.class);
+		 * startActivity(newActivity); } } });
+		 */
+
+		// btnCancel (Cancel)
+		final Button cancel = (Button) findViewById(R.id.btnCancel);
+		cancel.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Open Form Main
+				Intent newActivity = new Intent(AddActivity.this,
+						InventoryActivity.class);
+				startActivity(newActivity);
+				finish();
+			}
+		});
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.add_screen, menu);
+		return true;
 	}
 
 }
--- src/domain/SaleListItem.java
+++ src/domain/SaleListItem.java
@@ -1,13 +1,59 @@
 package domain;
 
 public class SaleListItem {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private String productName;
+	private int productQuan;
+	private int productPrice;
+	private String productID;
+	
+	public SaleListItem(String productName, int productQuan,
+			int productPrice,String productID) 
+	{
+		this.productID = productID;
+		this.productName = productName;
+		this.productQuan = productQuan;
+		this.productPrice = productPrice;
 	}
 
+
+	public String getProductID() {
+
+		return productID;
+	}
+
+	public void setProductID(String Productid) {
+
+		 this.productID = Productid;
+	}
+	
+	public String getProductName() {
+
+		return productName;
+	}
+
+	public void setProductName(String Productname) {
+
+		 this.productName = Productname;
+	}
+	
+	public int getProductQuan() {
+
+		return productQuan;
+	}
+	
+	public void setProductQuan(int ProductQuan) {
+
+		this.productQuan = ProductQuan;
+	}
+
+	public int getProductPrice() {
+
+		return productPrice;
+	}
+	public void setProductPrice(int ProductPrice) {
+
+		 this.productPrice = ProductPrice;
+	}
+	
 }
