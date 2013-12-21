aade488a6a29e9f5e118fc361f3ee24a8ce55f5a
Nopphadon Wangmethakul <w_lufe71@hotmail.com>
2013-12-21 15:04:19 +0700

    initial

---
+++ src/com/example/pointofsale/ProductCatalogActivity.java
@@ -0,0 +1,13 @@
+package com.example.pointofsale;
+
+public class ProductCatalogActivity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/domain/SaleReport.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class SaleReport {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
